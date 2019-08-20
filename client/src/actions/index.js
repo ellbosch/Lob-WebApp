6da55e7e234975f0e79b1cf1fbf7420c85a88a01
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2019-08-19 19:27:58 -0700

    changes to actions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,9 +1,42 @@
-// action types
-export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
+// // action types
+// export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
 
-// other constants
-export const VisibilityFilters = {
-    SHOW_ALL: 'SHOW_ALL',
-    SHOW_NBA: 'nba',
-    SHOW_NFL: 'nfl'
+// // other constants
+// export const VisibilityFilters = {
+//     SHOW_ALL: 'SHOW_ALL',
+//     SHOW_NBA: 'nba',
+//     SHOW_NFL: 'nfl'
+// }
+
+
+// action for selecting channel
+export const SELECT_CHANNEL = 'SELECT_CHANNEL';
+
+export const selectChannel = (channel) => {
+    return {
+        type: SELECT_CHANNEL,
+        channel
+    }
+}
+
+// action for requesting video posts
+export const REQUEST_POSTS = 'REQUEST_POSTS'
+
+const requestPosts = (channel) => {
+  return {
+    type: REQUEST_POSTS,
+    channel
+  }
+}
+
+// acton for receiving posts
+export const RECEIVE_POSTS = 'RECEIVE_POSTS'
+
+const receivePosts = (channel, json) => {
+  return {
+    type: RECEIVE_POSTS,
+    channel,
+    posts: json.data.children.map(child => child.data),
+    receivedAt: Date.now()
+  }
 }
--- client/src/reducers/videoPosts.js
+++ client/src/reducers/videoPosts.js
@@ -1,13 +1,30 @@
-import { TOGGLE_CHANNEL } from './actions';
+import { SELECT_CHANNEL, REQUEST_POSTS, RECEIVE_POSTS } from './actions';
 
 // reducer composition for channels
-function channel(state=null, action) {
+function videoPosts(
+    state={
+        isFetching: false,
+        items: []
+    }, action) {
+
     switch (action.type) {
-        case TOGGLE_CHANNEL:
-            return action.channel;
+        // case SELECT_CHANNEL:
+        //     return Object.assign({}, state, {
+        //         isFetching: true
+        //     });
+        case REQUEST_POSTS:
+            return Object.assign({}, state, {
+                isFetching: true
+            });
+        case RECEIVE_POSTS:
+            return Object.assign({}, state, {
+                isFetching: false,
+                items: action.posts,
+                lastUpdate: action.received_at
+            });
         default:
             return state;
     }
 }
 
-export default channel;
+export default videoPosts;
