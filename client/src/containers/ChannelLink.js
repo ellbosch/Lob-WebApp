e611b3271e161dd65155e918b7c738eb192420a4
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2019-08-22 18:55:02 -0700

    fixed bug that showed wrong url for all posts

--- client/src/containers/ChannelLink.js
+++ client/src/containers/ChannelLink.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 import { setVisibilityFilter } from '../actions';
 
 const ChannelLink = ({ channel, children }) => (
-    <NavLink exact to={channel === 'SHOW_ALL' ? '/' : `/${channel}`}>
+    <NavLink exact to={channel === 'SELECT_ALL' ? '/' : `/${channel}`}>
         {children}
     </NavLink>
 );
